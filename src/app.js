0fc8ae9b26c4e3c8011a78d6d8288ef6f92437f0
avhis-hek7 <sharmaavishek679@gmail.com>
2026-02-04 12:57:18 +0545

    upadte data in the server

--- src/app.js
+++ src/app.js
@@ -30,8 +30,20 @@ app.delete('/notes/:index',(req,res) => {
     delete notes[index]
 
     res.status(200).json({
-        
+
         mesaage:"notes deleted successfully"
+    })
+})
+
+app.patch('/notes/:index', (req,res) =>{
+
+    const index = req.params.index
+    const description = req.body.description
+
+    notes[index].description = description
+
+    res.status(200).json({
+        message:"otes description updated"
     })
 })
 
