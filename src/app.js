7c110ffdd07570af5d144e089935e79069aa836b
avhis-hek7 <sharmaavishek679@gmail.com>
2026-02-05 11:42:53 +0545

    implementing the imagekit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const postModel = require('./models/post.model');
 const multer = require('multer');
+const uploadFile = require('./services/storage.service')
 
 
 const app = express();
@@ -8,8 +9,12 @@ app.use(express.json());
 const upload = multer({storage:multer.memoryStorage()});
 
 
-app.post('/create-post', async(req,res) => {
+app.post('/create-post', upload.single("image"), async(req,res) => {
     console.log(req.body)
+    console.log(req.file)
+
+    const result = await uploadFile(req.file.buffer)
+    console.log(result)
 
 })
 
