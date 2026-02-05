ce4f0924668eec0cd99cea2710419bff143db4bc
avhis-hek7 <sharmaavishek679@gmail.com>
2026-02-05 13:58:36 +0545

    develop full stack application

---
+++ Frontend/src/pages/Feed.jsx
@@ -0,0 +1,45 @@
+import React, { useState,useEffect } from 'react'
+import axios from 'axios';
+
+
+
+const Feed = () => {
+    const [posts, setPosts] = useState([
+
+    ])
+
+    useEffect(()=>{
+
+        axios.get("http://localhost:3000/posts")
+        .then((res)=>{
+            setPosts(res.data.post)
+        })
+
+    },[])
+
+
+
+
+  return (
+    <section className='feed-section'>
+        {
+            posts.length > 0 ? (
+                posts.map((post)=>(
+                    <div key={post._id} className='post-card'>
+                        <img src={post.image} alt={post.caption} />
+                        <p>{post.caption}</p>
+
+                    </div>
+                ))
+            ) : (
+                <h1>No posts Available</h1>
+            )
+        }
+
+
+
+    </section>
+  )
+}
+
+export default Feed
