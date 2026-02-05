66afc8d1237980a4996533cd263adcecc4210e96
avhis-hek7 <sharmaavishek679@gmail.com>
2026-02-05 10:26:53 +0545

    start a project

--- src/app.js
+++ src/app.js
@@ -1,94 +1,10 @@
 const express = require('express');
-const noteModel = require('./models/note.model')
-
-const app = express();
-app.use(express.json())
+const app = express()
 
 
-/*
-POST/notes => create a notes
-GET/notes => get all notes
-DELETE/notes/:id => delete a notes
-PATCH/notes/:id => updated a notes
-
-*/
-
-app.post('/notes', async(req,res) => {
-
-    const data = req.body
-    await noteModel.create({
-
-        title: data.title,
-        description: data.description
-    })
-    res.status(201).json({
-        message: "Notes create successfully"
-    })
-
-
-})
-
-
-app.get('/notes', async(req, res) => {
-
-    const notes = await noteModel.find()
-
-    res.status(200).json({
-        message:"Fetching all notes from the backend",
-        notes: notes
-    })
-
-
-})
-
-
-
-// app.get('/notes', async(req, res) => {
-
-//     const notes = await noteModel.findOne({  find onlyone from the database which title is "test_title"
-//         title:"test_title"
-//     })
-
-//     res.status(200).json({
-//         message:"Fetching all notes from the backend",
-//         notes: notes
-//     })
-
-
-// })
-
-app.delete('/notes/:id', async(req,res) => {
-
-    const id = req.params.id
-
-    await  noteModel.findByIdAndDelete({
-        _id:id
-    })
-
-    res.status(200).json({
-        message:"Note deleted successfully"
-    })
-
-})
-
-
-app.patch('/notes/:id', async(req,res) => {
-
-    const id = req.params.id
-    const description = req.body.description
-
-    await noteModel.findOneAndUpdate(
-        
-        {_id:id},
-        {
-        description: description
-    })
-
-    res.status(200).json({
-        message:"notes updated successfully"
-    })
-})
 
 
 
 module.exports = app
+
+
